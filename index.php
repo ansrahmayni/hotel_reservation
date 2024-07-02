e0852a1027ceeedaa7f81624f81570275d4fd7fb
ansrahmayni <ansrahmayni@gmail.com>
2024-07-02 12:03:05 +0700

    mengganti h1

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Business & Family Hotel</title>
+    <title>BISNIS</title>
     <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
 </head>
 <body>
