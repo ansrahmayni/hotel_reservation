02d8004fd84228c36fe578d0687149c4aa191cd5
qewstaayy <annacalista904@gmail.com>
2024-07-02 12:31:46 +0700

    hai ngubah ni

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BISNIS</title>
+    <title>hai cok</title>
     <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
 </head>
 <body>
